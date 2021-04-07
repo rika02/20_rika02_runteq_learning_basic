d81f6b01f5f9db0d71cd38491c4d24f7e847a717
りか <rika@rikanoMacBook-Air.local>
2021-04-07 23:17:55 +0900

    Add: 変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,9 @@
 Rails.application.routes.draw do
   root to: 'static_pages#top'
-<<<<<<< HEAD
-  resources :static_pages
-
-  root to: 'users#index'
-  resources :users
-
-  get 'login' => 'user_sessions#new', :as => :login
-  post 'login' => 'user_sessions#create'
-  post 'logout' => 'user_sessions#destroy', :as => :logout
-=======
 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
 
   resources :users, only: %i[new create]
->>>>>>> f71a5e6... Add: i18nの設定の追加
 end
