0d6162e524b4ed0da887fe0d2f1e5cb803d92f5a
りか <rika@rikanoMacBook-Air.local>
2021-04-13 22:22:23 +0900

    Add: boardテーブルに関する設定の追加

---
+++ db/migrate/20210413131252_create_boards.rb
@@ -0,0 +1,10 @@
+class CreateBoards < ActiveRecord::Migration[5.2]
+  def change
+    create_table :boards do |t|
+      t.text :title, null: false
+      t.text :body, null: false
+
+      t.timestamps
+    end
+  end
+end
