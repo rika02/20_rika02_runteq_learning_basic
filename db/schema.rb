135462f08d7575b937995fcb53e79777262c4752
りか <rika@rikanoMacBook-Air.local>
2021-04-13 22:23:27 +0900

    Add: boardsテーブルの追加による変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_07_113749) do
+ActiveRecord::Schema.define(version: 2021_04_13_131252) do
+
+  create_table "boards", force: :cascade do |t|
+    t.text "title", null: false
+    t.text "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
