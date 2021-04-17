f467b1f2fc128bbbf5c286c45e78510deed4b092
りか <rika@rikanoMacBook-Air.local>
2021-04-17 22:38:54 +0900

    Add: ダミーデータの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+20.times do |n|
+  title = Faker::Food.sushi
+  body = Faker::Food.dish
+  user = User.find(User.ids.sample)
+  Board.create!(title: title,
+                body: body,
+                user: user
+                )
+end
