966e82460dcb379b27d2d074bd88096be72cde4c
りか <rika@rikanoMacBook-Air.local>
2021-04-17 22:41:35 +0900

    Add: マイグレーションファイルの追加によるファイルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,10 @@ ActiveRecord::Schema.define(version: 2021_04_13_131252) do
   create_table "boards", force: :cascade do |t|
     t.text "title", null: false
     t.text "body", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_boards_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
