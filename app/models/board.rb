e760b50701c00edae31d27426b5243dc9698a7fa
りか <rika@rikanoMacBook-Air.local>
2021-04-23 21:00:40 +0900

    Add: 掲示板作成のビューの追加

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,7 +1,7 @@
 class Board < ApplicationRecord
   belongs_to :user
 
-  validates :title, :body, presence: true,
-                           length: { maximum: 255 }
+  validates :title, :body, presence: true
+  validates :title, length: { maximum: 255 }
   validates :body, length: { maximum: 65_535 }
 end
