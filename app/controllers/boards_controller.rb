b546e7a64b0f7d614298ba83823dc41dab31804e
りか <rika@rikanoMacBook-Air.local>
2021-04-26 17:00:53 +0900

    回答を見ての修正

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,6 +1,6 @@
 class BoardsController < ApplicationController
   def index
-    @boards = Board.all.includes(:user)
+    @boards = Board.all.includes(:user).order(created_at: :desc)
   end
 
   def new
