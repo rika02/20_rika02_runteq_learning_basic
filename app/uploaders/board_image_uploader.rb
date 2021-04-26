c375a52c9dca8299e3cd39d5af57fddab9a84e4a
りか <rika@rikanoMacBook-Air.local>
2021-04-26 22:24:29 +0900

    lintチェックによる修正

--- app/uploaders/board_image_uploader.rb
+++ app/uploaders/board_image_uploader.rb
@@ -14,10 +14,10 @@ class BoardImageUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  def default_url(*args)
-  #   For Rails 3.1+ asset pipeline compatibility:
-    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  def default_url(*)
+  # For Rails 3.1+ asset pipeline compatibility:
+    ActionController::Base.helpers.asset_path('fallback/' + [version_name, 'default.png'].compact.join('_'))
+  # "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   end
 
   # Process files as they are uploaded:
@@ -35,7 +35,7 @@ class BoardImageUploader < CarrierWave::Uploader::Base
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_allowlist
-    %w(jpg jpeg gif png)
+    %w[jpg jpeg gif png]
   end
 
   # Override the filename of the uploaded files:
