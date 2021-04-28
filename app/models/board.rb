3a374a6cd62392fe161f2e8025360f2da08b84f6
りか <rika@rikanoMacBook-Air.local>
2021-04-28 18:51:44 +0900

    commentモデルとのアソシエーションの設定の追加

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,6 +1,7 @@
 class Board < ApplicationRecord
   mount_uploader :board_image, BoardImageUploader
   belongs_to :user
+  has_many :comments, dependent: :destroy
 
   validates :title, :body, presence: true
   validates :title, length: { maximum: 255 }
