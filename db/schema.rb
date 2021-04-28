3831c3fc85679ec9d73c9c68aa7903356f107af9
りか <rika@rikanoMacBook-Air.local>
2021-04-28 19:04:21 +0900

    commentテーブルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_26_102936) do
+ActiveRecord::Schema.define(version: 2021_04_27_153835) do
 
   create_table "boards", force: :cascade do |t|
     t.text "title", null: false
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 2021_04_26_102936) do
     t.datetime "updated_at", null: false
     t.string "board_image"
     t.index ["user_id"], name: "index_boards_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body", null: false
+    t.integer "board_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["board_id"], name: "index_comments_on_board_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
