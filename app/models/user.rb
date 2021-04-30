cad79b313a21742be00e9f040b3e7ba19faacf14
りか <rika@rikanoMacBook-Air.local>
2021-04-30 22:12:29 +0900

    課題１２の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,8 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates :last_name, :first_name, presence: true,
                                      length: { maximum: 255 }
+  
+  def own?(object)
+    id == object.user_id
+  end
 end
