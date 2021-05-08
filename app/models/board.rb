48cbbf557c52207120da3a083a9f8b2efa50c7f8
りか <rika@rikanoMacBook-Air.local>
2021-05-08 21:56:22 +0900

    モデルにアソシエーションの追加

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,6 +2,7 @@ class Board < ApplicationRecord
   mount_uploader :board_image, BoardImageUploader
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
 
   validates :title, :body, presence: true
   validates :title, length: { maximum: 255 }
