3b3eab4ad0f9cf81375ea291e6741bdd5e3b7aa7
りか <rika@rikanoMacBook-Air.local>
2021-05-08 21:55:24 +0900

    ブックマーク機能の追加

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,13 @@
+class BookmarksController < ApplicationController
+  def create
+    board = Board.find(params[:board_id])
+    current_user.bookmark(board)
+    redirect_back fallback_location: root_path, success: t('defaults.message.bookmarked', item: Bookmark.model_name.human) 
+  end
+
+  def destroy
+    board = current_user.bookmarks.find(params[:id]).board
+    current_user.unbookmark(board)
+    redirect_back fallback_location: root_path, success: t('defaults.message.delete_bookmarked', item: Bookmark.model_name.human)
+  end
+end
