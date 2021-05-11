caa3e70ad3394fdf4baa788649735b65fb271bfb
りか <rika@rikanoMacBook-Air.local>
2021-05-11 18:40:05 +0900

    ボタンのajax化

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,13 +1,11 @@
 class BookmarksController < ApplicationController
   def create
-    board = Board.find(params[:board_id])
-    current_user.bookmark(board)
-    redirect_back fallback_location: root_path, success: t('defaults.message.bookmarked', item: Bookmark.model_name.human)
+    @board = Board.find(params[:board_id])
+    current_user.bookmark(@board)
   end
 
   def destroy
-    board = current_user.bookmarks.find(params[:id]).board
-    current_user.unbookmark(board)
-    redirect_back fallback_location: root_path, success: t('defaults.message.delete_bookmarked', item: Bookmark.model_name.human)
+    @board = current_user.bookmarks.find(params[:id]).board
+    current_user.unbookmark(@board)
   end
 end
