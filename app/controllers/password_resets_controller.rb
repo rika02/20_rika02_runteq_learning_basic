d995e8557a0c26e2802ab62fb5b4c975e7b4518a
りか <rika@rikanoMacBook-Air.local>
2021-06-04 20:01:08 +0900

    lintチェックの修正

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -5,7 +5,7 @@ class PasswordResetsController < ApplicationController
   def new; end
 
   def create
-    @user = User.find_by(params[:email])
+    @user = User.find_by_email(params[:email])
     @user&.deliver_reset_password_instructions!
     redirect_to login_path, success: t('defaults.message.reset_password', item: User.human_attribute_name('password'))
   end
